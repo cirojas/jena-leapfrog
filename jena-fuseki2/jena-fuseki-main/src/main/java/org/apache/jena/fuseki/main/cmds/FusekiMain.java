1607011124ecea86cae27a5902fc49e6a5515924
Carlos Rojas <cirojas6@uc.cl>
2019-01-01 06:05:01 -0300

    added ARQ.init() to fuseki to prevent a weird bug

--- jena-fuseki2/jena-fuseki-main/src/main/java/org/apache/jena/fuseki/main/cmds/FusekiMain.java
+++ jena-fuseki2/jena-fuseki-main/src/main/java/org/apache/jena/fuseki/main/cmds/FusekiMain.java
@@ -102,6 +102,7 @@ public class FusekiMain extends CmdARQ {
 
         static void innerMain(String... argv) {
             JenaSystem.init();
+            ARQ.init();
             new FusekiMain(argv).mainRun();
         }
 
